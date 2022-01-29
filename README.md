dfd05c6810b3d86d9194e5cbf36ab12e2220d30b
迷渡 <justjavac@gmail.com>
2022-01-29 15:12:59 +0800

    please use yarn

---
+++ README.md
@@ -0,0 +1,25 @@
+# please-use-yarn
+
+> 强制在项目中使用 yarn 作为包管理器
+
+## 使用
+
+在 `package.json` 文件的 scripts 中添加 `preinstall`：
+
+```json
+{
+  "scripts": {
+    "preinstall": "npx please-use-yarn"
+  }
+}
+```
+
+## 效果
+
+当使用 `npm i` 安装依赖时，会报错并停止安装。
+
+
+
+## License
+
+[MIT](LICENSE)
---
+++ bin.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+if (!/yarn\.js$|yarnpkg$/.test(process.env.npm_execpath)) {
+  console.error(`\x1B[1;31m${"*".repeat(40)}\x1B[0;0m`);
+  console.error(`\x1B[1;31m* 请使用 yarn 安装依赖。\x1B[0;0m`);
+  console.error(`\x1B[1;31m${"*".repeat(40)}\x1B[0;0m`);
+  console.error(``);
+  process.exit(1);
+}
